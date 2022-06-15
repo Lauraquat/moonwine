c59af4bcfc00ab423554d5ae8f09dd4ca215461b
Lauraquat <lauraquat@gmail.com>
2022-06-15 16:46:44 +0200

    ajout plugins

---
+++ wp-content/plugins/subscriptions-for-woocommerce/build/index.asset.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Silence is golden.
+ *
+ * @link       https://wpswing.com/
+ * @since      1.0.0
+ *
+ * @package    Subscriptions_For_Woocommerce
+ */
+
+return array(
+	'dependencies' => array( 'wp-element', 'wp-polyfill' ),
+	'version' => 'b95adb316dfea9d9044a518aaa748969',
+);
